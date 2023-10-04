eebb0abba4d13f4512e1589440b6e8aff226f971
Knosper <j.jesberger@yahoo.com>
2023-10-04 12:42:32 +0200

    final

---
+++ client.c
@@ -0,0 +1,44 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    char buffer[4096];
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+	}
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(89);
+    addr.sin_addr.s_addr = htonl(2130706433);
+    if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("Connection failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Connected to the server.\n");
+    // Send a message to the server
+    char msg[42];
+    sprintf(msg, "Hello, Server!\nHallo\n\n\n");
+    int bytes = send(sockfd, msg, strlen(msg), 0);
+    printf("Message sent: %s\nbytes = %i\n", msg, bytes);
+    // Close the socket and exit
+    close(sockfd);
+    printf("Connection closed.\n");
+    return (0);
+}
+
--- mini_serv.c
+++ mini_serv.c
@@ -4,15 +4,27 @@
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/select.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/select.h>
 #include <signal.h>
+#include <stdlib.h>
 
-#define MAX_CLIENTS     128
-#define BUFFER_SIZE     42
+#define MAX_CLIENTS 128
+#define BUFFER_SIZE 3
 
-char msg[BUFFER_SIZE + BUFFER_SIZE];
+char msg[55000];
+
+typedef struct s_id
+{
+    int id;
+    int fd;
+    int msg;
+}   t_id;
+
+void fatal(const char* s)
+{
+    write(2, s, strlen(s));
+}
 
 int handler(int sig)
 {
@@ -22,82 +34,128 @@ int handler(int sig)
     return (ex);
 }
 
-void close_clients(int* id)
+void close_clients(t_id *id)
 {
     int i = 0;
     while (i < MAX_CLIENTS)
     {
-        if (id[i] >= 0)
-            close(id[i]);
+        if (id[i].fd >= 0)
+            close(id[i].fd);
         i++;
     }
 }
 
-int get_id(int fd, int* id)
+void send_all(int except, t_id *id, fd_set write_fd)
 {
     int i = 0;
     while (i < MAX_CLIENTS)
     {
-        if (id[i] == fd)
-            return (i);
-        i++;
-    }
-    return (-1);
-}
-
-void send_all(int except, int *id, fd_set write_fd)
-{
-    int i = 0;
-    while (i < MAX_CLIENTS)
-    {
-        if (id[i] >= 0 && id[i] != except)
+        if (id[i].fd >= 0 && id[i].fd != except)
         {
-            if (FD_ISSET(id[i], &write_fd))
+            if (FD_ISSET(id[i].fd, &write_fd))
             {
-                send(id[i], msg, strlen(msg), 0);
+                send(id[i].fd, msg, strlen(msg), 0);
             }
         }
         i++;
     }
 }
 
-void reset_id(int fd, int *id)
+void init_id(t_id *id)
 {
     int i = 0;
     while (i < MAX_CLIENTS)
     {
-        if (fd == id[i])
+        id[i].fd = -1;
+        id[i].msg = -1;
+        id[i].id = -1;
+        i++;
+    }
+}
+
+int get_id(int fd, t_id *id)
+{
+    int i = 0;
+    while (i < MAX_CLIENTS)
+    {
+        if (fd == id[i].fd)
+            return (id[i].id);
+        i++;
+    }
+    return (-1);
+}
+
+void reset_client(int fd, t_id *id)
+{
+    int i = 0;
+    while (i < MAX_CLIENTS)
+    {
+        if (fd == id[i].fd)
         {
-            id[i] = -1;
+            id[i].fd = -1;
+            id[i].msg = -1;
+            id[i].id = -1;
             break ;
         }
         i++;
     }
 }
 
-int get_max(int sockfd, int* id)
+int get_max(int sockfd, t_id *id)
 {
-    int max_fd = sockfd;
     int i = 0;
+    int max_fd = sockfd;
     while (i < MAX_CLIENTS)
     {
-        if (max_fd < id[i])
-            max_fd = id[i];
+        if (max_fd < id[i].fd)
+            max_fd = id[i].fd;
         i++;
     }
     return (max_fd);
 }
 
+int get_msg_flag(int fd, t_id* id)
+{
+    int i = 0;
+    while (i < MAX_CLIENTS)
+    {
+        if (fd == id[i].fd)
+            return (id[i].msg);
+        i++;
+    }
+    return (0);
+}
+
+void set_id_flag(int fd, t_id* id, int status)
+{
+    int i = 0;
+    while (i < MAX_CLIENTS)
+    {
+        if (fd == id[i].fd)
+        {
+            id[i].msg = status;
+            break ;
+        }
+        i++;
+    }
+}
+
 int main(int argc, char** argv)
 {
+    if (argc != 2)
+    {
+        fatal("wrong args!\n");
+        exit(1);
+    }
 	int sockfd, connfd, len;
 	struct sockaddr_in servaddr, cli; 
 
 	// socket create and verification 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (sockfd == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
+	if (sockfd == -1)
+    { 
+        fatal("socket creation failed...\n"); 
+		exit(1); 
 	} 
 	else
 		printf("Socket successfully created..\n"); 
@@ -109,35 +167,38 @@ int main(int argc, char** argv)
 	servaddr.sin_port = htons(atoi(argv[1])); 
   
 	// Binding newly created socket to given IP and verification 
-	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
-		printf("socket bind failed...\n"); 
-		exit(0); 
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
+    {
+        fatal("socket bind failed...\n");
+		exit(1); 
 	} 
 	else
 		printf("Socket successfully binded..\n");
-	if (listen(sockfd, 10) != 0) {
-		printf("cannot listen\n"); 
-		exit(0); 
+	if (listen(sockfd, MAX_CLIENTS) != 0)
+    {
+		fatal("cannot listen\n"); 
+		exit(1); 
 	}
-    signal(SIGINT, (void*)handler);
-    int id[MAX_CLIENTS];
-    int id_flag[MAX_CLIENTS];
-    memset(id_flag, 0, sizeof(id));
-    memset(id, -1, sizeof(id));
-    int max_fd = sockfd;
+	signal(SIGINT, (void*)handler);
     fd_set read_fd, write_fd, curr_fd;
     FD_ZERO(&curr_fd);
     FD_SET(sockfd, &curr_fd);
+    int max_fd = sockfd;
+    t_id id[MAX_CLIENTS];
+    init_id(id);
     int g_id = 0;
+ 
     while (1)
     {
         read_fd = write_fd = curr_fd;
         if (handler(0) == 42)
         {
+            fatal("shutdown server\n");
             break ;
         }
         if (select(max_fd + 1, &read_fd, &write_fd, 0, 0) < 0)
         {
+            fatal("select failed, continue...\n");
             continue ;
         }
         int fd = 0;
@@ -148,23 +209,24 @@ int main(int argc, char** argv)
                 if (sockfd == fd)
                 {
                     struct sockaddr_in client;
-                    int len = sizeof(client);
+                    socklen_t len = sizeof(client);
                     int client_fd = accept(fd, (struct sockaddr*)&client, &len);
                     if (client_fd >= 0)
                     {
                         FD_SET(client_fd, &curr_fd);
-                        id[g_id++] = client_fd;
+                        id[g_id].id = g_id;
+                        id[g_id].msg = 0;
+                        id[g_id++].fd = client_fd;
                         if (client_fd > max_fd)
                             max_fd = client_fd;
-                        sprintf(msg, "server: client %d just arrived\n", get_id(client_fd, id));
+                        sprintf(msg, "server: client %d just arrived\n", g_id - 1);
                         send_all(client_fd, id, write_fd);
+                        break ;                    
                     }
                 }
                 else
                 {
                     char buffer[BUFFER_SIZE + 1];
-                    int i = 0;
-                    int j = 0;
                     int ret = recv(fd, buffer, BUFFER_SIZE, 0);
                     if (ret <= 0)
                     {
@@ -172,41 +234,40 @@ int main(int argc, char** argv)
                         send_all(fd, id, write_fd);
                         FD_CLR(fd, &curr_fd);
                         close(fd);
-                        reset_id(fd, id);
+                        reset_client(fd, id);
                         max_fd = get_max(sockfd, id);
-                        break;
+                        break ;
                     }
                     else
                     {
-                        char str[BUFFER_SIZE + 2];
+                        buffer[ret] = '\0';
+                        int i = 0;
+                        int j = 0;
+                        char buff[ret + 2];
                         while (i < ret)
                         {
-                            str[j] = buffer[i];
-                            if (str[j] == '\n')
+                            buff[j] = buffer[i];
+                            if (buff[j] == '\n')
                             {
-                                str[j + 1] = '\0';
-                                if (id_flag[get_id(fd, id)])
-                                {
-                                    sprintf(msg, "%s", str);
-                                }
+                                buff[j + 1] = '\0';
+                                if (!get_msg_flag(fd, id))
+                                    sprintf(msg, "client %d: %s", get_id(fd, id), buff);
                                 else
-                                {
-                                    sprintf(msg, "client %d: %s", get_id(fd, id), str);
-                                }
-                                id_flag[get_id(fd, id)] = 0;
+                                    sprintf(msg, "%s", buff);
+                                set_id_flag(fd, id, 0);
                                 send_all(fd, id, write_fd);
                                 j = -1;
                             }
                             else if (i == ret - 1)
                             {
-                                str[j + 1] = '\0';
-                                if (id_flag[get_id(fd, id)])
+                                buff[j + 1] = '\0';
+                                if (get_msg_flag(fd, id))
                                 {
-                                    sprintf(msg, "%s", str);
+                                    sprintf(msg, "%s", buff);
                                 }
                                 else
-                                    sprintf(msg, "client %d: %s", get_id(fd, id), str);
-                                id_flag[get_id(fd, id)] = 1;
+                                    sprintf(msg, "client %d: %s", get_id(fd, id), buff);
+                                set_id_flag(fd, id, 1);
                                 send_all(fd, id, write_fd);
                                 break ;
                             }
@@ -219,7 +280,6 @@ int main(int argc, char** argv)
             fd++;
         }
     }
-
 
     close_clients(id);
     close(sockfd);
